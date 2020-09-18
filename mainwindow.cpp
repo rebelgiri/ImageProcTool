62b1bacb46ac1194612a645b3d83efe81250baca
Giriraj Pawar <giriraj.pawar@s2018.tu-chemnitz.de>
2020-09-18 11:38:25 +0200

    Implement color conversion RGB to HSV Code and Save CSV

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -92,39 +92,151 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
     QGraphicsSceneMouseEvent *mouseEvent = static_cast<QGraphicsSceneMouseEvent*>(event);
 
     QPointF img_coord_pt = mouseEvent->scenePos();
+    QPoint img_coord_pt_const = mouseEvent->screenPos();
 
     double x = img_coord_pt.x();
     double y = img_coord_pt.y();
 
-    QColor color = QColor(image.toImage().pixel(x,y));
+
+    //QColor color = QColor(image.toImage().pixel(x,y));
     //int average = (color.red()+color.green()+color.blue())/3;
 
     //ui->lineEdit->setText(QString::number(x));
     //ui->lineEdit_2->setText(QString::number(y));
    // ui->label_Value->setText(QString::number(average));
 
-
     // RGB Color
     QRgb rgbValue = image.toImage().pixel(x,y);
     QString rgbValueToDisply = QString::number(qRed(rgbValue)) + " " +
                                QString::number(qGreen(rgbValue)) + " " +
                                QString::number(qBlue(rgbValue)) ;
-    ui->rgb_color_values->setText(rgbValueToDisply);
 
+    /*ui->rgb_color_values->setText(rgbValueToDisply);*/
+
+    QToolTip::showText(img_coord_pt_const,rgbValueToDisply,this,rect());
+
+    /*
     // HSV Color
     QString hsvValueToDisply = QString::number(color.hue()) + " " +
                                QString::number(color.saturation())+ " " +
                                QString::number(color.value()) ;
-     ui->hsv_color_values->setText(hsvValueToDisply);
+     ui->hsv_color_values->setText(hsvValueToDisply);*/
 
      // LAB Color Conversion
-
-
-
-
 
     return true;
   } else {
     return QObject::eventFilter(obj, event);
   }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    // Save all the entries in CSV file
+
+    QString csvFileName = "color_values.csv";
+
+    /*QString csvFilePath = QFileDialog::getSaveFileName(
+                    this,
+                    tr("Save File"),
+                    "",
+                    tr("CSV (*.csv)" )
+                    );*/
+
+
+
+    QFile file(csvFileName);
+    if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text))
+    {
+        // We're going to streaming text to the file
+        QTextStream stream(&file);
+
+        stream << ui->rgb_color_values->text() << "," << ui->hsv_color_values->text()
+               << "," << ui->lab_color_values->text() << "\n";
+
+
+
+        file.close();
+
+        QMessageBox::information(this, tr("Image Processing Tool"), tr("CVS File Saved"));
+    }
+
+}
+
+void MainWindow::on_rgb_color_values_returnPressed()
+{
+    display_colors_on_text_edit_hsv_lab();
+}
+
+void MainWindow::on_hsv_color_values_returnPressed()
+{
+    display_colors_on_text_edit_rgb_lab();
+}
+
+void MainWindow::on_lab_color_values_returnPressed()
+{
+    display_colors_on_text_edit_rgb_hsv();
+}
+
+void MainWindow::display_colors_on_text_edit_hsv_lab()
+{
+    QString RGBValue = ui->rgb_color_values->text();
+
+    const QRegExp rx(QLatin1Literal("[^0-9]+"));
+    const auto&& parts = RGBValue.split(rx, QString::SkipEmptyParts);
+
+    float r = parts[0].toInt() / 255.0f;
+    float g = parts[1].toInt() / 255.0f;
+    float b = parts[2].toInt() / 255.0f;
+
+    float h, s, v; // h:0-360.0, s:0.0-1.0, v:0.0-1.0
+
+    float max = max_f(r, g, b);
+    float min = min_f(r, g, b);
+
+    v = max;
+
+    if (max == 0.0f) {
+        s = 0;
+        h = 0;
+    }
+    else if (max - min == 0.0f) {
+        s = 0;
+        h = 0;
+    }
+    else {
+        s = (max - min) / max;
+
+        if (max == r) {
+            h = 60 * ((g - b) / (max - min)) + 0;
+        }
+        else if (max == g) {
+            h = 60 * ((b - r) / (max - min)) + 120;
+        }
+        else {
+            h = 60 * ((r - g) / (max - min)) + 240;
+        }
+    }
+
+    if (h < 0) h += 360.0f;
+
+    unsigned char dst_h = (unsigned char)(h / 2);   // dst_h : 0-180
+    unsigned char dst_s = (unsigned char)(s * 255); // dst_s : 0-255
+    unsigned char dst_v = (unsigned char)(v * 255); // dst_v : 0-255
+
+    QString hsvValueToDisply = QString::number(dst_h) + " " +
+                               QString::number(dst_s) + " " +
+                               QString::number(dst_v) ;
+
+    ui->hsv_color_values->setText(hsvValueToDisply);
+
+}
+void MainWindow::display_colors_on_text_edit_rgb_hsv()
+{
+
+}
+void MainWindow::display_colors_on_text_edit_rgb_lab()
+{
+
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QGraphicsScene>
 #include <QGraphicsItem>
+#include <QToolTip>
 #include <QGraphicsSceneMouseEvent>
 #include "ui_mainwindow.h"
 #include "QFileDialog"
@@ -14,11 +15,22 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2\core\core.hpp"
+#include <QList>
+#include <QStringList>
+#include <QDir>
+#include <QDebug>
+
+
+
+
 using namespace cv;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+#define min_f(a, b, c)  (fminf(a, fminf(b, c)))
+#define max_f(a, b, c)  (fmaxf(a, fmaxf(b, c)))
 
 class MainWindow : public QMainWindow
 {
@@ -29,10 +41,21 @@ public:
     ~MainWindow();
     void open_dir();
     bool eventFilter(QObject *obj, QEvent *event);
+    void display_colors_on_text_edit_hsv_lab();
+    void display_colors_on_text_edit_rgb_hsv();
+    void display_colors_on_text_edit_rgb_lab();
 
 private slots:
     void on_open_dir_clicked();
     void on_save_image_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_rgb_color_values_returnPressed();
+
+    void on_hsv_color_values_returnPressed();
+
+    void on_lab_color_values_returnPressed();
 
 private:
     Ui::MainWindow *ui;
